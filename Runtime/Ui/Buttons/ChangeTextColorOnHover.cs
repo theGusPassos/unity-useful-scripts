407be41aa95c16805f7f57a6a48d5fea1b56676d
theguspassos <theguspassos@gmail.com>
2021-07-30 23:44:38 -0300

    add change text color on hover

---
+++ Runtime/Ui/Buttons/ChangeTextColorOnHover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Packages.unity_useful_scripts.Runtime.Ui.Buttons
+{
+    public class ChangeTextColorOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] Text textMesh;
+        [SerializeField] Color onHoverColor;
+        [SerializeField] float timeBetweenColors;
+        float timer;
+        Color targetColor;
+        Color originalColor;
+
+        void Start()
+        {
+            originalColor = targetColor =  textMesh.color;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            targetColor = onHoverColor;
+            timer = 0;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            targetColor = originalColor;
+            timer = 0;
+        }
+
+        void Update()
+        {
+            if (textMesh.color != targetColor)
+            {
+                timer += Time.deltaTime;
+                textMesh.color = Color.Lerp(textMesh.color, targetColor, timer / timeBetweenColors);
+            }
+        }
+    }
+}
