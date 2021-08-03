b8df8e29fdab27c43f8be067b31ba0967ab6d224
theguspassos <theguspassos@gmail.com>
2021-08-02 21:02:36 -0300

    add singleton class

---
+++ Runtime/Systems/SingletonMonoBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Packages.unity_useful_scripts.Runtime.Systems
+{
+    public class Singleton<T> : MonoBehaviour
+        where T : Component
+    {
+        public static T Instance;
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = GetComponent<T>();
+                return;
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
