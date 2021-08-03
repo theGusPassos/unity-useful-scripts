bd1b58f6d8a50643135723468cae8511e2f3cbaf
theguspassos <theguspassos@gmail.com>
2021-08-02 21:15:46 -0300

    add load scene on click component

---
+++ Runtime/Scenes/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Packages.unity_useful_scripts.Runtime.Scenes
+{
+    public class LoadSceneOnClick : MonoBehaviour, IPointerDownHandler
+    {
+        [SerializeField] string sceneToLoad;
+
+        public void OnPointerDown(PointerEventData eventData)
+            => SceneLoader.Instance.LoadScene(sceneToLoad);
+    }
+}
