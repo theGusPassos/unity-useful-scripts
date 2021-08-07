a5a7a677e89387c8b7692c6a6219857efa64a7f5
theguspassos <theguspassos@gmail.com>
2021-08-06 22:29:42 -0300

    add awakers

---
+++ Runtime/Ui/Awakers/SetImageMaxAlphaOnAwake.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Packages.unity_useful_scripts.Runtime.Ui.Awakers
+{
+    public class SetImageMaxAlphaOnAwake : MonoBehaviour
+    {
+        void Awake()
+        {
+            GetComponent<Image>().color += new Color(0, 0, 0, 1);
+        }
+    }
+}
