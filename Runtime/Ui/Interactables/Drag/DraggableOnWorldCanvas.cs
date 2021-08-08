28d13dae0f6ccb7005c1593e3ef6e7c2e9938a4e
theguspassos <theguspassos@gmail.com>
2021-08-08 15:33:15 -0300

    add draggable for world canvas

---
+++ Runtime/Ui/Interactables/Drag/DraggableOnWorldCanvas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Packages.unity_useful_scripts.Runtime.Ui.Interactables.Drag
+{
+    public class DraggableOnWorldCanvas : MonoBehaviour, IDragHandler
+    {
+        RectTransform rectTransform;
+        [SerializeField] RectTransform canvasRectTransform;
+
+        void Awake() => rectTransform = GetComponent<RectTransform>();
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvasRectTransform,
+                Input.mousePosition, 
+                Camera.main, 
+                out Vector2 local);
+
+            rectTransform.anchoredPosition = local;
+        }
+    }
+}
