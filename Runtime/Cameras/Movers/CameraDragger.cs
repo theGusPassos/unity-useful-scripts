5c2e6a1bd2bae1586067297c67661be4b5ce1f7c
theguspassos <theguspassos@gmail.com>
2021-08-08 15:53:26 -0300

    save starting camera drag

---
+++ Runtime/Cameras/Movers/CameraDragger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Packages.unity_useful_scripts.Runtime.Cameras.Movers
+{
+    public class CameraDragger : MonoBehaviour
+    {
+        [SerializeField] float dragSpeed;
+        Vector3 dragOrigin;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(2))
+            {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(2)) return;
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+            transform.Translate(move, Space.World);
+        }
+    }
+}
