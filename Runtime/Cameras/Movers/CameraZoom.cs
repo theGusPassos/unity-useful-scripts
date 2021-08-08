5f6e48a4d626775057dcaa67eeca5ba5dc622653
theguspassos <theguspassos@gmail.com>
2021-08-08 16:01:00 -0300

    add camera zoom

---
+++ Runtime/Cameras/Movers/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Packages.unity_useful_scripts.Runtime.Cameras.Movers
+{
+    public class CameraZoom : MonoBehaviour
+    {
+        [SerializeField] float speed;
+        [SerializeField] float minZoom;
+        [SerializeField] float maxZoom;
+        Camera cam;
+
+        void Awake()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        void LateUpdate()
+        {
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                cam.orthographicSize += Input.mouseScrollDelta.y * speed;
+
+                if (cam.orthographicSize > maxZoom)
+                    cam.orthographicSize = maxZoom;
+                else if (cam.orthographicSize < minZoom)
+                    cam.orthographicSize = minZoom;
+            }
+        }
+    }
+}
