e989d09047428ae433a56d678e5c8b6f0a830b93
theguspassos <theguspassos@gmail.com>
2021-08-08 19:22:48 -0300

    fix camera dragger

--- Runtime/Cameras/Movers/CameraDragger.cs
+++ Runtime/Cameras/Movers/CameraDragger.cs
@@ -5,21 +5,26 @@ namespace Packages.unity_useful_scripts.Runtime.Cameras.Movers
     public class CameraDragger : MonoBehaviour
     {
         [SerializeField] float dragSpeed;
-        Vector3 dragOrigin;
+        Vector3 hitPosition;
+        Vector3 currentPosition;
+        Vector3 cameraPosition;
 
         void Update()
         {
             if (Input.GetMouseButtonDown(2))
             {
-                dragOrigin = Input.mousePosition;
-                return;
+                hitPosition = Input.mousePosition;
+                cameraPosition = transform.position;
             }
 
-            if (!Input.GetMouseButton(2)) return;
-
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
-            transform.Translate(move, Space.World);
+            if (Input.GetMouseButton(2))
+            {
+                currentPosition = Input.mousePosition;
+                Vector3 direction = Camera.main.ScreenToWorldPoint(currentPosition) 
+                    - Camera.main.ScreenToWorldPoint(hitPosition);
+                direction *= -1;
+                transform.position = cameraPosition + direction;
+            }
         }
     }
 }
