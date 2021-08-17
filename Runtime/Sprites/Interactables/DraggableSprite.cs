70d63b9d34d93ef617662c549c22e592a2f4559b
theguspassos <theguspassos@gmail.com>
2021-08-16 21:29:59 -0300

    fix script for drag

---
+++ Runtime/Sprites/Interactables/DraggableSprite.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Packages.unity_useful_scripts.Runtime.Sprites.Interactables
+{
+    public class DraggableSprite : MonoBehaviour
+    {
+        Vector3 screenPoint;
+        Vector3 offset;
+
+        void OnMouseDown()
+        {
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+
+        void OnMouseDrag()
+        {
+            var curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            var curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+    }
+}
