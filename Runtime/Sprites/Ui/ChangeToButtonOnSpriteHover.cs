625feb4ba97ae3c5f900d713665b225b49579c88
theguspassos <theguspassos@gmail.com>
2021-08-28 15:58:14 -0300

    add cursor changer to sprite

---
+++ Runtime/Sprites/Ui/ChangeToButtonOnSpriteHover.cs
@@ -0,0 +1,14 @@
+﻿using Packages.unity_useful_scripts.Runtime.Utility.CursorUtils;
+using UnityEngine;
+
+namespace Packages.unity_useful_scripts.Runtime.Sprites.Ui
+{
+    public class ChangeToButtonOnSpriteHover : MonoBehaviour
+    {
+        void OnMouseEnter()
+            => CursorChanger.Instance.ShowOnButtonCursor();
+
+        void OnMouseExit()
+            => CursorChanger.Instance.ShowOnButtonCursor();
+    }
+}
