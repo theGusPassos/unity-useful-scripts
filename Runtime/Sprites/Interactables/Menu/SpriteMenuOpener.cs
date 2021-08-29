f9208bd5d494d4011187f67dd60e6007ae02efdf
theguspassos <theguspassos@gmail.com>
2021-08-29 18:49:16 -0300

    add sprite menu opener

---
+++ Runtime/Sprites/Interactables/Menu/SpriteMenuOpener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Packages.unity_useful_scripts.Runtime.Sprites.Interactables.Menu
+{
+    public class SpriteMenuOpener : MonoBehaviour
+    {
+        Vector3 positionWhenClicking;
+
+        void OnMouseDown()
+            => positionWhenClicking = transform.position;
+
+        void OnMouseUp()
+        {
+            if (positionWhenClicking != transform.position)
+                return;
+
+            print("clicked");
+        }
+    }
+}
